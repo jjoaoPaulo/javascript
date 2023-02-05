e7e6313b625d783ae7356ee21702894f04aec6c3
João Paulo Martins dos Santos <118926757+jjoaoPaulo@users.noreply.github.com>
2023-02-05 17:25:16 -0300

    s

---
+++ aula11/ex007.js
@@ -0,0 +1,7 @@
+var vel = 100.1
+if(vel >= 100){
+    console.log(`A velocidade do seu carro é ${vel}Km/h, tu foi mutado chefe`)
+}else{
+    console.log(`A velocidade do seu carro é ${vel}Km/h, tu ta dentro do limite manow`)
+}
+
