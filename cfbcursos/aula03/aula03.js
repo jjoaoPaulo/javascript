a888d2706f018aee2e89dd19c1e311ae0f3653e7
João Paulo Martins dos Santos <118926757+jjoaoPaulo@users.noreply.github.com>
2023-02-09 19:29:40 -0300

    h

---
+++ cfbcursos/aula03/aula03.js
@@ -0,0 +1,17 @@
+"use strict"
+
+function teste(){
+    let nome="Bruno"
+    if(true){
+        console.log("dentro do IF do teste" + nome)
+    }
+    console.log("dentro do teste" + nome)
+}
+
+teste()
+
+const nome1 = "cleiton";
+
+nome1 = "rochele";
+
+console.log(nome1)
---
+++ cfbcursos/aula10/aula10.js
@@ -0,0 +1,5 @@
+let num=10
+res=(!(num%2) ? "Par" : "Ímpar")
+console.log(res)
+
+//Teste lógico ? se verdadeiro : se falso
